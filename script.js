e55d940234c1cb74bddb78aa45d1a8257c5bb0d5
ana <annnasantos4530@gmail.com>
2025-11-12 11:31:07 -0300

    nova funcionalidade

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 //BOTAO LETRAS MAIUSCULA
-window.document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("btnMaiuscula")
-    .addEventListener("click", function () {
-      let inputText = document.querySelector("#input-text").value;
-      document.querySelector("#input-Result").innerHTML = inputText.toUpperCase();
-    });
+// BOTÃO LETRAS MAIÚSCULAS
+document
+  .getElementById("btnMaiuscula")
+  .addEventListener("click", function () {
+    let inputText = document.querySelector("#input-text").value;
+    document.querySelector("#input-Result").value = inputText.toUpperCase();
+  });
 
-  //BOTAO LETRAS MINUSCULAS
-  document
-    .getElementById("btnMinuscula")
+// BOTÃO LETRAS MINÚSCULAS
+document
+  .getElementById("btnMinuscula")
+  .addEventListener("click", function () {
+    let inputText = document.querySelector("#input-text").value;
+    document.querySelector("#input-Result").value = inputText.toLowerCase();
+  });
+
+    //capitalizar palavras
+    document
+    .getElementById("btnCapitalizar")
     .addEventListener("click", function () {
-      let inputText = document.querySelector("#input-text").value;
-      document.querySelector("#input-Result").innerHTML = inputText.toLowerCase();
-    });
+        let inputText = document.querySelector("#input-text").value;
+        let textoCapitalizado = inputText.replace(/\b\w+/g, palavra =>
+        palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase()
+        );
+        document.querySelector("#input-Result").value = textoCapitalizado;
+  });
+
+
+//BTN LIMPAR
+const botaoLimpar = document.getElementById('btnLimpar');
+botaoLimpar.addEventListener('click', function() {
+    //  OS CAMPOS QUE QUER LIMPAAAAAAAAARR
+    document.getElementById('input-text').value = '';
+    document.getElementById('input-Result').value = '';
+
+
+     //chams a função p redefinir 
+     contarCaracteres(); 
 });
+
+
+//contar caracteres
+function contarCaracteres() {
+    //pegaaaaaaaa o input
+    const inputTexto = document.getElementById("input-text");
+    
+    // onde o conbtador eh exibido
+    const outputContador = document.getElementById("contador-caracteres");
+    
+    // pega o valor do input eh mede o tamanho
+    const numeroDeCaracteres = inputTexto.value.length;
+    
+    // mostra o resultado na tela
+    outputContador.textContent = `${numeroDeCaracteres} caracteres`;
+}
+
+//toda vez que o usuário digita algo
+document.getElementById("input-text").addEventListener("input", contarCaracteres);
 
